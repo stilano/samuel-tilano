77c6651d8c811e4998931024c22169feb2b05e40
sdtilano <144489063+stilano@users.noreply.github.com>
2026-09-21 09:52:07 -0500

    Update setup script for Linux tools and Python packages

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+set -e
 
+# Herramientas Linux
+sudo apt update
+sudo apt install -y \
+    time \
+    linux-tools-common \
+    linux-tools-generic
+
+# Permitir acceso a contadores de rendimiento
+sudo sysctl -w kernel.perf_event_paranoid=-1
+
+# Configurar perf
+sudo ln -sf \
+    /usr/lib/linux-tools-6.8.0-139/perf \
+    /usr/local/bin/perf
+
+# Paquetes Python
+python3 -m pip install -r .devcontainer/python_libraries_requirements.txt
